b1372b3bf0460714a4a79257927a6f60c228e757
bearbro <773854587@qq.com>
2017-12-28 20:29:04 +0800

    ejb-服务端

---
+++ src/cn/edu/zjut/ejb/UserSessionBean.java
@@ -0,0 +1,16 @@
+package cn.edu.zjut.ejb;
+
+import javax.ejb.Stateless;
+
+@Stateless(name = "UserService")
+public class UserSessionBean implements UserServiceRemote {
+    public UserSessionBean() {
+    }
+
+    @Override
+    public boolean login(String username, String password) {
+        if ("zjut".equals(username) && "zjut".equals(password))
+            return true;
+        return false;
+    }
+}
